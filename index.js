cf6dc35d6c5112dbb9ba18496aec580fc68c8ad2
nick.haynes <nick.haynes@selectquotesenior.com>
2017-02-10 10:10:13 -0600

    Updating JSDoc comments and adding default values so startIndex and stopIndex are optional params.

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ import {identifyTheNumberType, logTheNumberDetails} from './services/fizzBuzzSer
 
 /**
  * @function executeService
+ * @param {number} startIndex - the number you want to start from (optional: defaults to 1)
+ * @param {number} stopIndex - the last number you wish to evaluate (optional: defaults to 100)
+ * @example executeService(200, 10000);
  * @description
  *   This is just a helper function to fire off our Fizz Buzz service
  *   Basically all that is happening is the for loop runs and the numbers
  *   get evaluated and logged to the console.
  * */
-const executeService = (startIndex, stopIndex) => {
+const executeService = (startIndex = 1, stopIndex = 100) => {
 	for (let i = startIndex; i <= stopIndex; i++) {
 		logTheNumberDetails(identifyTheNumberType(i));
 	}
 };
 
-executeService(1, 100);
+executeService();
