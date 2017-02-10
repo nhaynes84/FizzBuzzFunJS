0e6c32761f40e2ebc1fce2cbb4b7434cfd1d97c4
nick.haynes <nick.haynes@selectquotesenior.com>
2017-02-10 09:53:13 -0600

    Removing unnecessary folder structure

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {identifyTheNumberType, logTheNumberDetails} from './src/services/fizzBuzzServices';
+import {identifyTheNumberType, logTheNumberDetails} from './services/fizzBuzzServices';
 
 for (let i = 1; i <= 100; i++) {
 	logTheNumberDetails(identifyTheNumberType(i));
